d53ac1e656c54325cbc8c9538977789c9cca09b0
alexmarkovskii <markovskiialex@gmail.com>
2024-04-02 02:37:50 +0300

    update

--- tests/QuadraticEquation_test.h
+++ tests/QuadraticEquation_test.h
@@ -12,39 +12,45 @@ extern "C" {
 #include "QuadraticEquation.h"
 }
 
-double roots[2];
+
 
 TEST(Quadratictest, two_roots){
+    double roots[2];
     solveQuadraticEquation(roots, 1, 4, -5);
     ASSERT_EQ(roots[0], -5);
     ASSERT_EQ(roots[1], 1);
 }
 
 TEST(Quadratictest, one_root){
+    double roots[2];
     solveQuadraticEquation(roots, 1, -6, 9);
     ASSERT_EQ(roots[0], 3);
     ASSERT_EQ(roots[1], 3);
 }
 
 TEST(Quadratictest, float_root){
+    double roots[2];
     solveQuadraticEquation(roots, 4, 6, 2);
     ASSERT_EQ(roots[0], -1);
     ASSERT_EQ(roots[1], -0.5);
 }
 
 TEST(Quadratictest, zero_discriminant){
+    double roots[2];
     solveQuadraticEquation(roots, 4, -5, 0);
     ASSERT_EQ(roots[0], 0);
     ASSERT_EQ(roots[1], 1.25);
 }
 
 TEST(Quadratictest, nan_roots){
+    double roots[2];
     solveQuadraticEquation(roots, 4, 2, 4);
     ASSERT_TRUE(isnan(roots[0]));
     ASSERT_TRUE(isnan(roots[1]));
 }
 
 TEST(Quadratictest, invalid_vars){
+    double roots[2];
     solveQuadraticEquation(roots, 0, -5, 4);
     ASSERT_TRUE(isnan(roots[0]));
     ASSERT_TRUE(isnan(roots[1]));
